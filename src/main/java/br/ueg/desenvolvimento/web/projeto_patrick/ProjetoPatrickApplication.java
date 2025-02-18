768d7f15cc21b5ff2a208f458ace9c50b6a56a40
Patrick Oliveira Vagetti <77699722+DevFalconsz@users.noreply.github.com>
2025-02-17 21:13:58 -0300

    Import inicial - 17/02/2025

---
+++ src/main/java/br/ueg/desenvolvimento/web/projeto_patrick/ProjetoPatrickApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.desenvolvimento.web.projeto_patrick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPatrickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPatrickApplication.class, args);
+	}
+
+}
